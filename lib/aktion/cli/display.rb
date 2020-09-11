47e1e44e6facdbf912623f680d118f651c013176
Rickard Sunden <rickard.sunden@outlook.com>
2020-09-10 22:22:31 -0400

    Prettier

--- lib/aktion/cli/display.rb
+++ lib/aktion/cli/display.rb
@@ -4,47 +4,43 @@ module Aktion
       attr_accessor :indent_count
       def initialize
         @indent_count = 0
-        Whirly.configure(
-          spinner: 'dots',
-          stop: '✅',
-          color: false
-        )
+        Whirly.configure(spinner: 'dots', stop: '✅', color: false)
       end
-  
+
       def self.start(&block)
         new.instance_eval(&block)
       end
-  
+
       def output
         STDOUT
       end
-  
+
       def tab_size
         2
       end
-  
+
       def tab_character
         ' '
       end
-  
+
       def tab
         tab_character * tab_size
       end
-  
+
       def indentation
         tab * indent_count
       end
-  
+
       def write(*args)
         output.puts("#{indentation}#{Paint[*args]}")
       end
-  
+
       def spinner(options = {})
         Whirly.start({ indentation: indentation }.merge(options)) do
           yield(Whirly)
         end
       end
-  
+
       def indent(times = 1, &block)
         self.indent_count += 1 * times
         yield
