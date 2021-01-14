1612cfe60947aae8a03279d1d96dfb55be50eae2
Rickard Sunden <rickardsunden@gmail.com>
2021-01-14 18:10:30 -0500

    Add progress

--- spec/examples/create.rb
+++ spec/examples/create.rb
@@ -12,6 +12,26 @@ class Create < Aktion::Base
     end
   end
 
+  contract do
+    params do
+      required(:current_user).filled
+      required(:params).hash do
+        required(:name).filled(:string)
+        option(:completed_at).maybe(:date)
+      end
+    end
+
+    rules do
+      rule(:current_user) do
+        error('username must be rickard') if value.name != 'rickard'
+      end
+    end
+
+    responses {}
+
+    examples {}
+  end
+
   def perform
     item = Item.new(item_params)
     item.save ? success(:ok, item) : failure(:unprocessable_entity, item.errors)
