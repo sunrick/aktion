3a6f0f9c9c983e3c28e63809634a7f983ce9bd1c
Rickard Sunden <rickardsunden@gmail.com>
2021-02-02 20:21:57 -0500

    Add smarter version of aktions

--- lib/aktion/controller.rb
+++ lib/aktion/controller.rb
@@ -1,17 +1,27 @@
 module Aktion
   module Controller
     module ClassMethods
+      # aktion_class_name = self.to_s.gsub('Controller', '')
+      # aktion_const = "#{aktion_class_name}::#{name.to_s.classify}".constantize
+
+      # view_class_name = "#{aktion_class_name}_component"
+      # view_const = "#{aktion_class_name}::#{view_class_name}".constantize
+
       def aktions(aktions)
-        aktions.each do |aktion|
-          aktion_class_name = self.to_s.gsub('Controller', '')
-          aktion_const =
-            "#{aktion_class_name}::#{aktion.to_s.classify}".constantize
+        aktions.each do |name, options|
+          klass_const = options[:action]
+          view_const = options[:view]
+          statuses = options[:statuses]
 
-          view_class_name = "#{aktion_class_name}_component"
-          view_const = "#{aktion_class_name}::#{view_class_name}".constantize
-
-          define_method aktion do
+          define_method name do
             instance = klass_const.perform(aktion_request)
+            status_handler = statuses[instance.status] if instance.failure? &&
+              instance.status
+
+            if status_handler == :raise && instance.status == :not_found
+              raise ActionController::RoutingError.new('Not Found')
+            end
+
             render view_const.new(instance.body), status: instance.status
           end
         end
