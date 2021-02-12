4c914d83f3cb9c91f225793d7fc38702078c8068
Rickard Sunden <rickardsunden@gmail.com>
2021-02-12 17:15:23 -0500

    More refactoring

--- benchmark/contracts/single_param.rb
+++ benchmark/contracts/single_param.rb
@@ -1,32 +1,28 @@
-module SingleParam
-  class User
-    include ActiveModel::Validations
+class User
+  include ActiveModel::Validations
 
-    attr_accessor :name
+  attr_accessor :name
 
-    validates :name, presence: true
+  validates :name, presence: true
 
-    def initialize(name:)
-      self.name = name
-    end
+  def initialize(name:)
+    self.name = name
   end
+end
 
-  dry =
-    Dry::Validation::Contract.build do
-      params { required(:name).filled(:string) }
-    end
+dry =
+  Dry::Validation::Contract.build { params { required(:name).filled(:string) } }
 
-  aktion = Aktion::Contract.build { request { required :name, :string } }
+aktion = Aktion::Contract.build { request { required :name, :string } }
 
-  params = { name: 'Rickard' }
+params = { name: 'Rickard' }
 
-  Benchmark.ips do |x|
-    x.report('active-model') do
-      user = User.new(params)
-      user.validate
-    end
-    x.report('dry-validation') { dry.call(params) }
-    x.report('aktion-request') { aktion.call(params) }
-    x.compare!
+Benchmark.ips do |x|
+  x.report('active-model') do
+    user = User.new(params)
+    user.validate
   end
+  x.report('dry-validation') { dry.call(params) }
+  x.report('aktion-request') { aktion.call(params) }
+  x.compare!
 end
