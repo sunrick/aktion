c470ab49e586584bd82c07a7d4b927a8e8d845be
Rickard Sunden <rickardsunden@gmail.com>
2021-02-16 12:19:25 -0500

    Add array specs

--- spec/examples/types_spec.rb
+++ spec/examples/types_spec.rb
@@ -135,3 +135,35 @@ RSpec.describe Aktion::Types::Hash do
     end
   end
 end
+
+RSpec.describe Aktion::Types::Array do
+  context '.call' do
+    [
+      [nil, nil, TypesSpec::MISSING],
+      ['', '', TypesSpec::INVALID],
+      ['a', 'a', TypesSpec::INVALID],
+      [1, 1, TypesSpec::INVALID],
+      ['1', '1', TypesSpec::INVALID],
+      [1.12, 1.12, TypesSpec::INVALID],
+      ['1.12', '1.12', TypesSpec::INVALID],
+      [BigDecimal('1.55'), BigDecimal('1.55'), TypesSpec::INVALID],
+      [Date.parse('2021-01-01'), Date.parse('2021-01-01'), TypesSpec::INVALID],
+      [
+        DateTime.parse('2021-01-01'),
+        Date.parse('2021-01-01'),
+        TypesSpec::INVALID
+      ],
+      [Time.parse('2021-01-01'), Time.parse('2021-01-01'), TypesSpec::INVALID],
+      [true, true, TypesSpec::INVALID],
+      [false, false, TypesSpec::INVALID],
+      [{}, {}, TypesSpec::INVALID],
+      [{ a: 1 }, { a: 1 }, TypesSpec::INVALID],
+      [[], [], TypesSpec::MISSING],
+      [[1], [1], TypesSpec::OK],
+      ['true', 'true', TypesSpec::INVALID],
+      ['false', 'false', TypesSpec::INVALID]
+    ].each do |input, output, message|
+      specify { expect(described_class.call(input)).to eq([output, message]) }
+    end
+  end
+end
