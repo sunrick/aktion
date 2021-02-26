e5b9c406acf098fedc1a172a10eeb2a7e8979598
Rickard Sunden <rickardsunden@gmail.com>
2021-02-26 14:32:56 -0500

    Passing specs

--- spec/aktion/messages_spec.rb
+++ spec/aktion/messages_spec.rb
@@ -2,15 +2,29 @@ require 'spec_helper'
 require 'i18n'
 
 RSpec.describe Aktion::Messages do
-  context 'default' do
+  context 'hash' do
+    specify { expect(Aktion::Messages.backend).to eq(Aktion::Messages::Hash) }
+
+    specify do
+      expect(Aktion::Messages.backend.translate(:missing)).to eq('is missing')
+    end
+
+    specify do
+      expect(Aktion::Messages.backend.translate(:invalid)).to eq('invalid type')
+    end
   end
 
   context 'i18n' do
     before { Aktion::Messages.backend = :i18n }
 
+    specify { expect(Aktion::Messages.backend).to eq(Aktion::Messages::I18n) }
+
     specify do
-      expect(Aktion::Messages.backend).to eq(Aktion::Messages::I18n)
       expect(Aktion::Messages.backend.translate(:missing)).to eq('is missing')
+    end
+
+    specify do
+      expect(Aktion::Messages.backend.translate(:invalid)).to eq('invalid type')
     end
   end
 end
